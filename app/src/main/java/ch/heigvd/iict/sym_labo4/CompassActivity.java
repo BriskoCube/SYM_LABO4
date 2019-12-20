34695bfd50bde30974e206ac207a9bafffa05240
nathan.seville <nathan.seville@hotmail.ch>
2019-12-20 10:58:38 +0100

    Adding names to code

--- app/src/main/java/ch/heigvd/iict/sym_labo4/CompassActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym_labo4/CompassActivity.java
@@ -1,3 +1,10 @@
+/**
+ * Julien Quartier & Nathan Séville
+ *
+ * Date: 20.12.2019
+ * Description: Activité boussole
+ */
+
 package ch.heigvd.iict.sym_labo4;
 
 import android.hardware.Sensor;
@@ -10,21 +17,20 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 import ch.heigvd.iict.sym_labo4.gl.OpenGLRenderer;
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
 
-    //opengl
+    // opengl
     private OpenGLRenderer  opglr           = null;
     private GLSurfaceView   m3DView         = null;
 
+    // sensors
     private SensorManager sensorManager     = null;
     private Sensor accelerometer            = null;
     private Sensor magnetic                 = null;
 
+    // sensor & rotation matrix data
     private float[] gravity                 = null;
     private float[] geomagnetic             = null;
 
@@ -47,19 +53,22 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         // link to GUI
         this.m3DView = findViewById(R.id.compass_opengl);
 
-        //init opengl surface view
+        // init opengl surface view
         this.m3DView.setRenderer(this.opglr);
 
+        // get sensors
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
+        // register listners on sensors
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
         sensorManager.registerListener(this, magnetic, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        // Actualise les données en fonction du sensor
         switch (sensorEvent.sensor.getType()) {
             case Sensor.TYPE_ACCELEROMETER:
                 this.gravity = sensorEvent.values;
@@ -70,6 +79,7 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
                 break;
         }
 
+        // Actualise l'affichage si tout les parametres sont présents
         if(this.gravity != null && this.geomagnetic != null && SensorManager.getRotationMatrix(this.rotationMatrix, null, this.gravity, this.geomagnetic)) {
             this.rotationMatrix = this.opglr.swapRotMatrix(this.rotationMatrix);
         }
